b72a2a975e3355904c4dd007e94c0751c642e277
miagerman <49194476@est.ort.edu.ar>
2025-06-10 18:59:06 -0300

    .

--- Assets/DeteccionDeColisiones.cs
+++ Assets/DeteccionDeColisiones.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DeteccionDeColisiones : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void OnCollisionEnter(Collision collision)
+   {
+        Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+   }
 }
+
+
+
+
