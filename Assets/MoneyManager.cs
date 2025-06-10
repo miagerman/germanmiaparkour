846059768d2d55f1924503e094e63aa12a3a805f
miagerman <49194476@est.ort.edu.ar>
2025-06-10 19:22:52 -0300

    .

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIMANAGER uiMANAGER;
+    void Start()
+    {
+        uiMANAGER.UpdateMoneyTxt(money.ToString());
+    }
+
+    public void UpdateMoney(float moneyToAdd)
+    {
+        if (money + moneyToAdd < 0)
+        {
+            Debug.Log("No te alcanza");
+            return;
+        }
+        money += moneyToAdd;
+        uiMANAGER.UpdateMoneyTxt(money.ToString());
+    }
+}
+
