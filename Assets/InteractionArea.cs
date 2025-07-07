b743d3822d97f27b3ee1bb4189ce92b22bd69fe6
miagerman <49194476@est.ort.edu.ar>
2025-07-07 10:11:38 -0300

    .

--- Assets/InteractionArea.cs
+++ Assets/InteractionArea.cs
@@ -7,35 +7,34 @@ public class InteractionArea : MonoBehaviour
     public GameObject interactionMessage;
     public MercanciaScript mercancia;
 
-
+    // Start is called before the first frame update
     void Start()
     {
         interactionMessage.SetActive(false);
     }
 
+    // Update is called once per frame
     void Update()
     {
-    if (Input.GetKeyDown(KeyCode.E))
-    {
-        if (mercancia)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Destroy(mercancia.gameObject);
-            StopInteraction();
+            if (mercancia)
+            {
+                Destroy(mercancia.gameObject);
+                StopInteraction();
+            }
         }
     }
-}
-
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
+        mercancia = other.GetComponent<MercanciaScript>();
 
-    MercanciaScript mercancia = other.GetComponent<MercanciaScript>();
-
-    if (mercancia)
-    {
-        interactionMessage.SetActive(true);
-    }
+        if (mercancia)
+        {
+            interactionMessage.SetActive(true);
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -43,10 +42,9 @@ public class InteractionArea : MonoBehaviour
         StopInteraction();
     }
 
-     void StopInteraction()
+    void StopInteraction()
     {
         interactionMessage.SetActive(false);
         mercancia = null;
     }
-
 }
